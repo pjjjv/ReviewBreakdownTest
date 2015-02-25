b89a5554f29e8d127f677741395718d5206a2eec
pjv <ezelspinguin@gmail.com>
2015-02-26 00:58:01 +0100

    First version

---
+++ web/demo.dart
@@ -0,0 +1,60 @@
+/*
+ *  Review Breakdown Test - dart
+ *  Copyright (c) 2015 pjv
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+import 'package:template_binding/template_binding.dart';
+
+void main() {
+  initPolymer().run(() {
+
+    Polymer.onReady.then((_) {
+      var autoBind = document.querySelector('#auto-bind');
+      templateBind(autoBind).model = new MyModel();
+    });
+  });
+}
+
+
+class MyModel extends Observable {
+  @observable List<Map> items = toObservable([{
+      'label': 'Design',
+      'heat': 0
+    }, {
+      'label': 'Display',
+      'heat': 1
+    }, {
+      'label': 'Camera',
+      'heat': 6
+    }, {
+      'label': 'Reception / Call Quality',
+      'heat': 4
+    }, {
+      'label': 'Performance',
+      'heat': 7
+    }, {
+      'label': 'Software',
+      'heat': 10
+    }, {
+      'label': 'Battery Life',
+      'heat': 9
+    }, {
+      'label': 'Ecosystem',
+      'heat': 8
+    },]);
+}
